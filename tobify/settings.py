79b35aeb132783dd5721b069c6e2cd051df66446
Your Name <you@example.com>
2024-03-23 23:04:01 +0530

    hey

--- tobify/settings.py
+++ tobify/settings.py
@@ -26,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jw4x0jyxn##2crsck+=w-owzs!3-@5bs@jo#pom4*$_x_@io3!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['tobify-repo-production.up.railway.app',
-                 'tobix.dev', '127.0.0.1', 'www.tobix.dev']
+ALLOWED_HOSTS = ['tobix.dev', '127.0.0.1', 'www.tobix.dev']
 
+CSRF_TRUSTED_ORIGINS = ["https://tobix.dev", "https://tobix.dev"]
 
 # Application definition
 
